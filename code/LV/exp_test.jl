95615fb2d9176a69a82bf8a23803087e152bc455
DaniDuan <d.duan20@imperial.ac.uk>
2023-01-11 18:11:54 +0000

    currently not replicating experimental data

--- code/LV/exp_test.jl
+++ code/LV/exp_test.jl
@@ -11,41 +11,37 @@ r12 = data_r[:,:B0]
 Ea_r = data_r[:,:Ea]
 Ed_r = fill(4, 3)
 Th_r = data_r[1:3,:Th] .+ 273.15 # from data
-r0_all = temp_func((10+273.15), r12, Ea_r, Ed_r, Th_r) 
+r0_all = temp_func((10+273.15), (12+273.15), r12, Ea_r, Ed_r, Th_r) 
 
-α0_v = -data_a[:,:B0]
 row_Eα = data_a[:,:Ea]
 Ed_α = fill(4, 3, 3)
 row_Th = data_a[:,:Th]
-α0_all = temp_func((10+273.15), α0_v, row_Eα, Ed_α, row_Th)
+Ea_α = reshape(repeat(row_Eα, 3),(3,3))
+Th_α = [row_Th row_Th row_Th] .+ 273.15
+α0 = [-1.76997 2.92526 0.58057; -0.980159 -0.00855062 -0.648966; -0.618594 -1.25503 -0.777955]
+# Plots.heatmap(α0, yflip=true, c = :gist_yarg,ticks = false)
 
-# data_r = data_r[Not(2), :]
-# data_a = data_a[Not(2), :]
-
-######## S18 + W03 ########
+# initial data
 N = 2
+Tr = 10+273.15
 C0 = fill(0.01, N)
-tspan = (0,100)
-T = 10+273.15
+tspan = (0,24*24)
 
-# r0 = r0_all[1:N]
-r0 = r0_all[Not(1)]
-# α0ii = α0_all[1:N]
-α0ii = α0_all[Not(1)]
-
-# #----------interspecies interaction is random for now---------------#
-# α0_range = -α0ii*0.25
-# α0 = reduce(vcat, transpose.([rand(Normal(α0ii[i], α0_range[i]), N) for i in 1:N]))
-# #----------interspecies interaction is random for now---------------#
-# α0[diagind(α0)] = α0ii
-# Ea_α = reshape(repeat(row_Eα, N),(N,N))
-# Th_α = [row_Th row_Th row_Th] .+ 273.15
-# α = temp_func(T, α0, Ea_α, Ed_α, Th_α)
-
-# ## Simulation
-# p = (N = N, α = α, r = r)
-# prob = ODEProblem(GLV_model!, C0, tspan, p)
-# sol = solve(prob, Tsit5())
+for i in 1:3
+    T = 10*i +273.15
+    α = temp_func(T, Tr, α0, Ea_α, Ed_α, Th_α)
+    r = temp_func(T, Tr, r0_all, Ea_r, Ed_r, Th_r)
+    α_12 = α[Not(2),Not(2)]
+    r_12 = r[Not(2)]
+    # ## Simulation
+    p = (N = N, α = α_12, r = r_12)
+    prob = ODEProblem(GLV_model!, C0, tspan, p)
+    sol = solve(prob, Tsit5())
+    @eval $(Symbol("fig_temp$(10*i)")) = $(Plots.plot(sol, xaxis = "Time", yaxis = "Biomass", label=["W02" "W03"], legend=true, lc = [:blue :darkorange], title = string("Temp = ", i*10)))    
+end
+l = @layout [a b c]
+# color = darkgreen, blue, darkorange
+display(Plots.plot(fig_temp10, fig_temp20, fig_temp30, layout = l, size = (950, 300)))
 
 # ## Analysis
 # δT = -1/0.0000862 * (1/T - 1/(12+273.15))
@@ -60,64 +56,5 @@ r0 = r0_all[Not(1)]
 # "\nbiomass: ", sol.u[length(sol)], "\n", fst > 0, " ", snd > 0)
 
 # color = ("darkgreen", "blue", "chocolate2")
-# Plots.plot(sol, xaxis = "Time", yaxis = "Biomass", label=["S18" "W03"], legend=true, lc = [:darkgreen :darkorange])    
+Plots.plot(sol, xaxis = "Time", yaxis = "Biomass", label=["S18" "W02"], legend=true, lc = [:darkgreen :blue])    
 
-function LV(du, u, p, t)
-    # Model parameters.
-    α_ij, α_ji = p
-    # Current state.
-    x, y = u
-
-    # Evaluate differential equations.
-    du[1] = x*(r0[1]+α0ii[1]*x + α_ij*y)
-    du[2] = y*(r0[2]+α0ii[2]*x + α_ji*y)
-end
-
-odedata = hcat(data_ref[:,2], data_ref[:,3])
-
-time = [1, 24, 49, 72, 91]
-
-α_ij = -0.5
-α_ji = -1
-
-α_ij/α0ii[2] - r0[1]/r0[2] #<0
-α0ii[1]/α_ji - r0[1]/r0[2] #>0
-
-p = [α_ij α_ji]
-prob = ODEProblem(LV, C0, tspan, p)
-sol = solve(prob, Tsit5(); saveat=0.5)
-Plots.plot(sol)
-Plots.scatter!(time, odedata)
-
-@model function fitlv(data, prob)
-    # Prior distributions.
-    σ ~ InverseGamma(2, 3)
-    α_ij ~ truncated(Normal(-0.5, 0.5); lower=-1, upper=0)
-    α_ji ~ truncated(Normal(-1, 0.5); lower=-1.5, upper=-0.5)
-
-    # Simulate Lotka-Volterra model. 
-    p = [α_ij,α_ji]
-    predicted = solve(prob, Tsit5(); p=p, saveat=0.5)
-
-    # Observations.
-    if length(predicted) == 201
-        for i in 1:5
-            t = time[i]
-            data[i,:] ~ MvNormal(predicted[t], σ^2 * I)
-        end
-    end
-    return nothing
-end
-
-# data = Matrix{Union{Missing, Float64}}(missing, 101, 2)
-# for i in 1:5
-#     t = time[i]
-#     data[t,:] = odedata[i,:]
-# end
-
-model = fitlv(odedata, prob)
-
-# Sample 3 independent chains with forward-mode automatic differentiation (the default).
-chain = sample(model, NUTS(0.65), MCMCSerial(), 1000, 3; progress=false)
-
-Plots.plot(chain)
--- code/LV/temp_params.jl
+++ code/LV/temp_params.jl
@@ -1,25 +1,25 @@
 using Random, Distributions
 
-function temp_func(T, B0, Ea, Ed, Th)
+function temp_func(T, Tr, B0, Ea, Ed, Th)
     k =  0.0000862 # Boltzman constant
-    Tr = 12 + 273.15
     B = B0 .* exp.(-Ea/k .* (1/T.-1/Tr))# ./(1 .+ (Ea./(Ed - Ea)) .* exp.((Ed/k) .* (1 ./Th .- 1/T)))
     return B
 end
 
 function Params(N, T)
+    Tr = 10+273.15
     r0 = rand(Normal(0.21, 0.01), N) # from data
     Ea_r = rand(Normal(0.95, 0.16), N) # from data
     Ed_r = fill(4, N)
     Th_r = rand(Normal(25+273.15, 5), N) # from data
-    r = temp_func(T, r0, Ea_r, Ed_r, Th_r) 
+    r = temp_func(T, Tr, r0, Ea_r, Ed_r, Th_r) 
     # assuming every α has a seperated TPC
     α0 =  rand(Normal(-1.18, 0.5), N,N) # mean value from data, test variation
     ran_Eα = rand(Normal(2.2, 0.5), N)
     Ea_α = reshape(repeat(ran_Eα, N),(N,N)) # mean value from data, test variation
     Ed_α = fill(4, N, N)
     Th_α = rand(Normal(24+273.15, 1), N, N) # test variation
-    α = temp_func(T, α0, Ea_α, Ed_α, Th_α)
+    α = temp_func(T, Tr, α0, Ea_α, Ed_α, Th_α)
     return (N = N, α = α, r = r)
 end
 
